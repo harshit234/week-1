6c0a38647a26bebf6ed3439cf456d5ebf8b324e0
harshit234 <76657523+harshit234@users.noreply.github.com>
2026-02-25 15:35:50 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,6 +1,6 @@
-
 temperature = [32, 34, 33, 35, 36, 34, 33, 32, 31, 35]   
 humidity = [45, 50, 55, 60, 58, 52, 48, 47, 49, 53]      
+aqi = [110, 120, 130, 140, 135, 125, 115, 118, 122, 128] 
 
 def calculate_average(data):
     return sum(data) / len(data)
@@ -8,12 +8,12 @@ def calculate_average(data):
 def calculate_median(data):
     sorted_data = sorted(data)
     n = len(sorted_data)
-    
+
     if n % 2 == 0:
         median = (sorted_data[n//2 - 1] + sorted_data[n//2]) / 2
     else:
         median = sorted_data[n//2]
-    
+
     return median
 
 avg_temp = calculate_average(temperature)
@@ -22,12 +22,19 @@ median_temp = calculate_median(temperature)
 avg_humidity = calculate_average(humidity)
 median_humidity = calculate_median(humidity)
 
-print("📊 Weather Analysis for Gandhinagar (Last 10 Days)\n")
+avg_aqi = calculate_average(aqi)
+median_aqi = calculate_median(aqi)
+
+print("📊 Weather & AQI Analysis for Gandhinagar (Last 10 Days)\n")
 
 print("🌡 Temperature:")
-print("Average Temperature:", round(avg_temp, 2), "°C")
-print("Median Temperature:", median_temp, "°C\n")
+print("Average:", round(avg_temp, 2), "°C")
+print("Median :", median_temp, "°C\n")
 
 print("💧 Humidity:")
-print("Average Humidity:", round(avg_humidity, 2), "%")
-print("Median Humidity:", median_humidity, "%")
+print("Average:", round(avg_humidity, 2), "%")
+print("Median :", median_humidity, "%\n")
+
+print("🌫 AQI (Air Quality Index):")
+print("Average:", round(avg_aqi, 2))
+print("Median :", median_aqi)
